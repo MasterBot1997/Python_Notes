5d2c14c33e858f393407421abdb9e316bf609e9b
Ivan Belyaev <bel3187228@yandex.ru>
2023-05-02 01:00:59 +0300

    добавлена Функция выборки(сортировки по дате)
    Добавил функцию сортировки по дате добавления\редактирования
    - конкретная дата
    - год-месяц
    - год
    - в диапазонене года от до

--- function.py
+++ function.py
@@ -6,13 +6,15 @@ import os, fnmatch
 
 # Реализовать консольное приложение заметки, с
 
+
 def print_list_file(b):
     for i in range(len(b)):
         print(f"{i+1} - {b[i]}")
 
+
 # Печать заметки
 def print_file(name):
-    with open("all_notes/"+name, "r", encoding="utf8") as f:
+    with open("all_notes/" + name, "r", encoding="utf8") as f:
         reader = csv.reader(f)
         for row in reader:
             print(row)
@@ -21,7 +23,23 @@ def print_file(name):
 # функция чтением данных заметки с определенным id или по Заголовку если несколько одинаковых реализовано через словарь
 def prin_dict(filter, key, name_f):
     key = check_key(filter, key)
-    with open("all_notes/"+ name_f, "r", encoding="utf8") as f:
+    with open("all_notes/" + name_f, "r", encoding="utf8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            print(row["data_and_time"])
+            if row[filter] == key:
+                print(
+                    row["id"],
+                    row["header"],
+                    row["the_note"],
+                    row["data_and_time"],
+                    sep=" ; ",
+                )
+
+
+def filter_date(filter, key, name_f):
+    key = check_key(filter, key, name_f)
+    with open("all_notes/" + name_f, "r", encoding="utf8") as f:
         reader = csv.DictReader(f)
         for row in reader:
             if row[filter] == key:
@@ -33,7 +51,59 @@ def prin_dict(filter, key, name_f):
                     sep=" ; ",
                 )
 
-                # header,the_note,data_and_time
+
+def specific_filter_date(filter, key, name_f, n):
+    key = check_key_s(filter, key, name_f, n)
+    with open("all_notes/" + name_f, "r", encoding="utf8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if n == "1":
+                temp = row[filter].split(" ")[0]
+                if temp == key:
+                    print(
+                        row["id"],
+                        row["header"],
+                        row["the_note"],
+                        row["data_and_time"],
+                        sep=" ; ",
+                    )
+            elif n == "2":
+                # Сохраняем значение год и месяц
+                temp = (
+                    row[filter].split(" ")[0].split("-")[0]
+                    + "-"
+                    + row[filter].split(" ")[0].split("-")[1]
+                )
+                if temp == key:
+                    print(
+                        row["id"],
+                        row["header"],
+                        row["the_note"],
+                        row["data_and_time"],
+                        sep=" ; ",
+                    )
+            elif n == "3":
+                # Сохраняем значение года
+                temp = row[filter].split(" ")[0].split("-")[0]
+                if temp == key:
+                    print(
+                        row["id"],
+                        row["header"],
+                        row["the_note"],
+                        row["data_and_time"],
+                        sep=" ; ",
+                    )
+            elif n == "4":
+                # Сохраняем значение года
+                temp = row[filter].split(" ")[0].split("-")[0]
+                if int(temp) >= int(key.split("-")[0]) <= int(key.split("-")[1]):
+                    print(
+                        row["id"],
+                        row["header"],
+                        row["the_note"],
+                        row["data_and_time"],
+                        sep=" ; ",
+                    )
 
 
 # добавлением записи в файл
@@ -41,7 +111,7 @@ def new_entry(num, name):
     row = []
     current_datetime = datetime.now()
     num = num + 1
-    with open("all_notes/"+name, "a", encoding="utf8", newline="") as f:
+    with open("all_notes/" + name, "a", encoding="utf8", newline="") as f:
         header = input("Введите заголовок: ")
         the_note = input("Введите текст заметки: ")
         row.append(num)
@@ -60,18 +130,18 @@ def redactor_data(filter, name_f):
     temp = []
     key = check_delite_key(key)
     current_datetime = datetime.now()
-    with open("all_notes/"+name_f, "r", encoding="utf8") as f:
+    with open("all_notes/" + name_f, "r", encoding="utf8") as f:
         reader = csv.reader(f)
         for row in reader:
             temp.append(row)
-        with open("all_notes/"+name_f, "w", encoding="utf8", newline="") as t:
+        with open("all_notes/" + name_f, "w", encoding="utf8", newline="") as t:
             for i in range(len(temp)):
                 if key != temp[i][0]:
                     writer = csv.writer(t)
                     writer.writerow(temp[i])
                 else:
                     temp[i].pop(filter)
-                    temp[i].insert(filter, input('Введите новую информацию: '))
+                    temp[i].insert(filter, input("Введите новую информацию: "))
                     temp[i].pop(-1)
                     temp[i].append(current_datetime)
                     writer = csv.writer(t)
@@ -86,11 +156,11 @@ def delite_data(name):
     print()
     temp = []
     key = check_delite_key(key)
-    with open("all_notes/"+name, "r", encoding="utf8") as f:
+    with open("all_notes/" + name, "r", encoding="utf8") as f:
         reader = csv.reader(f)
         for row in reader:
             temp.append(row)
-        with open("all_notes/"+name, "w", encoding="utf8", newline="") as t:
+        with open("all_notes/" + name, "w", encoding="utf8", newline="") as t:
             for i in range(len(temp)):
                 if key != temp[i][0]:
                     writer = csv.writer(t)
@@ -100,30 +170,33 @@ def delite_data(name):
 
 # Оппределение последнего id
 def last_id(name):
-    with open("all_notes/"+name, "r", encoding="utf8") as f:
+    with open("all_notes/" + name, "r", encoding="utf8") as f:
         g = f.readlines()[-1].split(",")
         c = g[0]
         b = check_last_id(c)
         return b
 
+
 # Метод возвращающий все имена файлов заметок
 def save_name_notes():
     all_file = []
-    listOfFiles = os.listdir('all_notes/.')  
-    pattern = "*.csv"  
-    for entry in listOfFiles:  
+    listOfFiles = os.listdir("all_notes/.")
+    pattern = "*.csv"
+    for entry in listOfFiles:
         if fnmatch.fnmatch(entry, pattern):
-                all_file.append(entry)
+            all_file.append(entry)
     return all_file
+
 
 # Создание нового файла заметок
 def new_file(name):
-    row_zero = ["id","header","the_note","data_and_time"]
-    with open("all_notes/"+name+".csv", "w+", encoding="utf8") as f:
+    row_zero = ["id", "header", "the_note", "data_and_time"]
+    with open("all_notes/" + name + ".csv", "w+", encoding="utf8") as f:
         writer = csv.writer(f)
         writer.writerow(row_zero)
 
+
 # Удаление файла
 def delite_file(name):
-    path = "all_notes/"+name
+    path = "all_notes/" + name
     os.remove(path)
