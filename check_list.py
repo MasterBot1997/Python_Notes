b8a9967c222293dc4114b720888faf20fb70130c
Ivan Belyaev <bel3187228@yandex.ru>
2023-05-02 00:59:25 +0300

    Update check_list.py
    Добавлена функция проверки данных при использовании сортировки по дате добавления\редактирования заметки

--- check_list.py
+++ check_list.py
@@ -1,5 +1,7 @@
 from logg import logging
 import csv
+
+
 # Проверка является ли id первым или нет, на слуай нового списка
 def check_last_id(num):
     if num.isdigit():
@@ -10,20 +12,50 @@ def check_last_id(num):
         num = 0
         return int(num)
 
-# Проверка корректности ввода ключа, для полчучения данных о 
+
+# Проверка корректности ввода ключа, для полчучения данных о
 # пользователе через id
-def check_key(filter, num):
+def check_key(filter, num, name_f):
     while True:
-        with open("all_notes/text.csv", 'r',encoding='utf8') as f:
+        with open("all_notes/" + name_f, "r", encoding="utf8") as f:
             reader = csv.DictReader(f)
             for row in reader:
                 if row[filter] == num:
+                    return num
+                elif row[filter].split(' ')[0] == num:
+                    return num
+                elif row[filter].split(' ')[0].split('-')[0] +'-'+ row[filter].split(' ')[0].split('-')[1] == num:
+                    return num
+                elif row[filter].split(' ')[0].split('-')[0] == num:
                     return num
             else:
                 logging.warning("Введен несуществующий параметр")
                 print("\n'Такого параметра не существует!'")
                 num = input("Введите еще раз: ")
                 print()
+
+def check_key_s(filter, num, name_f, n):
+    while True:
+        with open("all_notes/" + name_f, "r", encoding="utf8") as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                if row[filter] == num and n == '1':
+                    return num
+                elif row[filter].split(' ')[0] == num:
+                    return num
+                elif row[filter].split(' ')[0].split('-')[0] +'-'+ row[filter].split(' ')[0].split('-')[1] == num and n == '2':
+                    return num
+                elif row[filter].split(' ')[0].split('-')[0] == num and n == '3':
+                    return num
+                elif  num.split('-')[0].isdigit() and num.split('-')[1].isdigit():
+                    if int(row[filter].split(' ')[0].split('-')[0]) >= int(num.split('-')[0]) <= int(num.split('-')[1]) and n == '4':
+                        return num
+            else:
+                logging.warning("Введен несуществующий параметр")
+                print("\n'Такого параметра не существует!'")
+                num = input("Введите еще раз: ")
+                print()
+
 
 # Проверка ключа id для удаления строки с информацией
 def check_delite_key(a):
@@ -42,22 +74,24 @@ def check_delite_key(a):
             a = input("Введите еще раз: ")
             print()
 
+
 def checking_for_files(all_file):
-    if len(all_file) == 0: 
-            print('Приетствую, у вас еще нет файлов с заметками.\nДавайте создадим новый!')
-            name_new_file = input('Введите имя нового файла без расширения: ')
-            row_zero = ["id","header","the_note","data_and_time"]
-            with open("all_notes/"+name_new_file+".csv", "w+", encoding="utf8") as f:
-                writer = csv.writer(f)
-                writer.writerow(row_zero)
-            logging.info(f"Еще нет файлов, создан новый файл заметок {name_new_file}")
+    if len(all_file) == 0:
+        print("Приетствую, у вас еще нет файлов с заметками.\nДавайте создадим новый!")
+        name_new_file = input("Введите имя нового файла без расширения: ")
+        row_zero = ["id", "header", "the_note", "data_and_time"]
+        with open("all_notes/" + name_new_file + ".csv", "w+", encoding="utf8") as f:
+            writer = csv.writer(f)
+            writer.writerow(row_zero)
+        logging.info(f"Еще нет файлов, создан новый файл заметок {name_new_file}")
     return True
 
-def check_command(a,b):
+
+def check_command(a, b):
     while True:
-        if (a.isnumeric() and int(a)>0<=b):
+        if a.isnumeric() and int(a) > 0 <= b:
             return int(a)
-        else:            
+        else:
             logging.warning("Некорректные данные!")
             print("\n'Неккоректные данные!'")
             a = input("Введите номер файла еще раз: ")
